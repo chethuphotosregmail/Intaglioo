b3365de8514cf5c93839b97d3c880fdaa70e0576
chethuphotosregmail <chethuphotosre@gmail.com>
2025-11-04 10:54:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,4 +191,8 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    from waitress import serve
+    print("🚀 Starting production server...")
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
